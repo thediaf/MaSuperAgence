207017c1533f0158f34bb9de9b289de85ffbf217
diafra <jaharacs98@gmail.com>
2020-02-12 01:28:21 +0000

    feat: frontend

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+    namespace App\Controller;
+
+use App\Repository\PropertyRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="home")
+     *
+     * @param PropertyRepository $repository
+     * @return Response
+     */
+    public function index(PropertyRepository $repository): Response
+    {
+        $properties = $repository->findLastest();
+
+        return $this->render('pages/home.html.twig', ['properties' => $properties]);
+    }
+}
